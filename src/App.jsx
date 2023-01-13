0a015df6bdc3d85bd1c78780729cce38bc0921e0
Moon Patel <moonpatel2003@gmail.com>
2023-01-14 02:00:50 +0530

    routes added .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Gallery from './components/Gallery';
 import Home from './components/home'
 
 function App() {
 
     return (
-                // <Route path="home" component={Home} /> 
-                <Home/>  
+        <Routes>
+            <Route path='/' element={<Navigate replace to={"/home"} />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/gallery' element={<Gallery />} />
+        </Routes>
     );
 }
 
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './home_component/Navbar'
+
+const Gallery = () => {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <div className='h-20'></div>
+            <div className='p-10 columns-2 gap-6 space-y-6'>
+                <img src="https://images.unsplash.com/photo-1613564834361-9436948817d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <img src="https://images.unsplash.com/photo-1615719413546-198b25453f85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <img src="https://images.unsplash.com/photo-1600028068383-ea11a7a101f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <img src="https://images.unsplash.com/photo-1571407970349-bc81e7e96d47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <img src="https://images.unsplash.com/photo-1611915365928-565c527a0590?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <img src="https://images.unsplash.com/photo-1622880833523-7cf1c0bd4296?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <img src="https://images.unsplash.com/photo-1584365685547-9a5fb6f3a70c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzJ8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <img src="https://images.unsplash.com/photo-1600628421066-f6bda6a7b976?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODF8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <img src="https://images.unsplash.com/photo-1607018244619-dab6235709dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTR8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <img src="https://images.unsplash.com/photo-1607929298871-fbdcf7e84852?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA3fHxwaXp6YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Gallery
