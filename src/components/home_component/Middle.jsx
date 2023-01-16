ce706cb44f9f2ed115c3acca6741c8a898071cef
Moon Patel <moonpatel2003@gmail.com>
2023-01-16 22:58:15 +0530

    ui fixes

--- src/components/home_component/Middle.jsx
+++ src/components/home_component/Middle.jsx
@@ -5,9 +5,9 @@ import LocationDetails from "../LocationDetails";
 export default function Middle() {
     return (
         <div>
-            <div className="h-[720px]">
-                <div className="h-[720px]">
-                    <img className="w-full h-[720px]" src="https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="img" />
+            <div className="">
+                <div className="">
+                    <img className="w-full" src="https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="img" />
                     {/* <img src="" alt="img" className="img" />
                     <img src="" alt="img" className="img" />
                     <img src="" alt="img" className="img" />
@@ -27,10 +27,10 @@ export default function Middle() {
             </div>
             <div className="menu my-10 text-center bg-gray-200">
                 <h1 className="text-3xl font-bold my-5">Our Menu</h1>
-                <div className="3-img flex">
-                    <img src="https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80" className="w-48 m-auto" />
-                    <img src="https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80" className="w-48 m-auto" />
-                    <img src="https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80" className="w-48 m-auto" />
+                <div className="flex">
+                    <img src="https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80" className="w-20 m-auto" />
+                    <img src="https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80" className="w-20 m-auto" />
+                    <img src="https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80" className="w-20 m-auto" />
                 </div>
                 <button className=" bg-black text-lg font-semibold text-white mx-36 mt-5 text-center">View Menu</button>
             </div>
