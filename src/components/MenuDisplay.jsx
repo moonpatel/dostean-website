fa6cd8ccf11cab7114888e1f2906cc962da77b1b
Jay <jaypatel4099@gmail.com>
2023-01-18 20:06:15 +0530

    special menu change

--- src/components/MenuDisplay.jsx
+++ src/components/MenuDisplay.jsx
@@ -9,74 +9,68 @@ const MenuDisplay = () => {
         <div className='h-full w-1/2 bg-cover bg-center bg-[url("../src/components/images/sp1.jpg")]'></div>
         <div className='w-1/2 bg-dark h-full p-3 text-left'>
           <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
-            Mexican Sizzler
+            Hot Chocolate with Biscuit Cup
           </h1>
-          <div className='text-gray-500 text-sm mb-5'>
-            Mexican rice served with curry, patty, sautéed veg and fries
-          </div>
-          <div className='text-2xl text-primary-200 font-bold mb-0'>₹499</div>
+          <div className='text-gray-500 text-sm mb-5'></div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹60</div>
         </div>
       </div>
       <div className='flex flex-row-reverse md:flex-row h-52'>
         <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp2.jpg")]'></div>
         <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
           <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
-            Hot Chocolate
+            Sukun wali chai
           </h1>
-          <div className='text-gray-500 text-sm mb-5'>
-            Hot Chocolate with Biscuit Cup
-          </div>
-          <div className='text-2xl text-primary-200 font-bold mb-0'>₹60</div>
+          <div className='text-gray-500 text-sm mb-5'></div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹45</div>
         </div>
       </div>
       <div className='flex md:flex-row-reverse lg:flex-row h-52'>
         <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp3.jpg")]'></div>
         <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
           <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
-            Burger
+            Green Manchurian
           </h1>
           <div className='text-gray-500 text-sm mb-5'>
-            Lorem commodi consequuntur nisi facilis, quo recusandae vero
-            perferendis officia!
+            Vegetable balls toasted with cheese spinach
           </div>
-          <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹189</div>
         </div>
       </div>
       <div className='flex flex-row-reverse h-52'>
         <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp4.jpg")]'></div>
         <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
           <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
-            Jeera Rice
+            Pulled Cheese Garlic Bread
           </h1>
           <div className='text-gray-500 text-sm mb-5'>
-            Hot aromatic rice with cumin tadka
+            House special garlic bread
           </div>
-          <div className='text-2xl text-primary-200 font-bold mb-0'>₹99</div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹209</div>
         </div>
       </div>
       <div className='flex lg:flex-row-reverse h-52'>
         <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp5.jpg")]'></div>
         <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
           <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
-            Plain-chatpata peri peri
+            Cilantro Rice With Garlic Sauce
           </h1>
           <div className='text-gray-500 text-sm mb-5'>
-            Fries with the choice of flavor
+            Flavored cilantro rice served with cheese burnt garlic sauce
           </div>
-          <div className='text-2xl text-primary-200 font-bold mb-0'>₹129</div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹249</div>
         </div>
       </div>
       <div className='flex flex-row-reverse md:flex-row lg:flex-row-reverse h-52'>
         <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp6.jpg")]'></div>
         <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
           <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
-            Hakka Noodles
+            Mexican Sizzler
           </h1>
           <div className='text-gray-500 text-sm mb-5'>
-            Hakka noodles are Indian-Chinese style noodles made with unleavened
-            refined wheat flour.
+            Mexican rice served with curry, patty, sautéed veg and fries
           </div>
-          <div className='text-2xl text-primary-200 font-bold mb-0'>₹169</div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹499</div>
         </div>
       </div>
     </div>
