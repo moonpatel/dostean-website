6b782f82eee66feb0b59111469c3a059ac9b1190
Moon Patel <moonpatel2003@gmail.com>
2023-01-18 11:29:42 +0530

    commit .

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ const Gallery = () => {
     return (
         <React.Fragment>
             <Navbar />
-            <div className='bg-blend-multiply bg-black/50 relative w-full h-[360px] md:h-[480px] bg-center bg-cover bg-[url(https://images.unsplash.com/photo-1522784081430-8ac6a122cbc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)]'>
+            <div className='bg-blend-multiply bg-black/50 relative w-full h-[360px] md:h-[480px] bg-center bg-cover -z-10 bg-[url(https://images.unsplash.com/photo-1522784081430-8ac6a122cbc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)]'>
                 <div className='absolute top-1/3 text-center w-full z-0'>
                     <h1 className='text-white text-4xl md:text-8xl fontmerienda my-2 md:my-5'>Image Gallery</h1>
                     <p className='text-primary-200 font-medium text-sm md:text-lg'>Get a glimpse of our authentic dishes.</p>
--- src/components/MenuDisplay.jsx
+++ src/components/MenuDisplay.jsx
@@ -6,7 +6,7 @@ const MenuDisplay = () => {
             <div className='flex h-52'>
                 <div className='h-full w-1/2 bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
                 </div>
-                <div className='w-1/2 bg-dark h-full p-3 text-left'>
+                <div className='w-1/2 bg-black h-full p-3 text-left'>
                     <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
                     <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
                     <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
@@ -15,7 +15,7 @@ const MenuDisplay = () => {
             <div className='flex flex-row-reverse md:flex-row h-52'>
                 <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
                 </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+                <div className='w-1/2 col-span-1 bg-black h-full p-3 text-left'>
                     <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
                     <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
                     <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
@@ -24,7 +24,7 @@ const MenuDisplay = () => {
             <div className='flex md:flex-row-reverse lg:flex-row h-52'>
                 <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
                 </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+                <div className='w-1/2 col-span-1 bg-black h-full p-3 text-left'>
                     <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
                     <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
                     <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
@@ -33,7 +33,7 @@ const MenuDisplay = () => {
             <div className='flex flex-row-reverse h-52'>
                 <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
                 </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+                <div className='w-1/2 col-span-1 bg-black h-full p-3 text-left'>
                     <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
                     <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
                     <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
@@ -42,7 +42,7 @@ const MenuDisplay = () => {
             <div className='flex lg:flex-row-reverse h-52'>
                 <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
                 </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+                <div className='w-1/2 col-span-1 bg-black h-full p-3 text-left'>
                     <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
                     <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
                     <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
@@ -51,7 +51,7 @@ const MenuDisplay = () => {
             <div className='flex flex-row-reverse md:flex-row lg:flex-row-reverse h-52'>
                 <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
                 </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+                <div className='w-1/2 col-span-1 bg-black h-full p-3 text-left'>
                     <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
                     <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
                     <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
