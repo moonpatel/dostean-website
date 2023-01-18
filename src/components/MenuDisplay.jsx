86980cf927ece2916388865c9c22a5b883d657b4
Jay <jaypatel4099@gmail.com>
2023-01-18 12:31:26 +0530

    menu display updated-1

--- src/components/MenuDisplay.jsx
+++ src/components/MenuDisplay.jsx
@@ -1,64 +1,86 @@
+/** @format */
+
 import React from 'react'
 
 const MenuDisplay = () => {
-    return (
-        <div className='grid grid-cols-1 my-3 md:grid-cols-2 lg:grid-cols-3'>
-            <div className='flex h-52'>
-                <div className='h-full w-1/2 bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
-                </div>
-                <div className='w-1/2 bg-dark h-full p-3 text-left'>
-                    <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
-                    <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
-                    <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
-                </div>
-            </div>
-            <div className='flex flex-row-reverse md:flex-row h-52'>
-                <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
-                </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
-                    <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
-                    <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
-                    <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
-                </div>
-            </div>
-            <div className='flex md:flex-row-reverse lg:flex-row h-52'>
-                <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
-                </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
-                    <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
-                    <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
-                    <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
-                </div>
-            </div>
-            <div className='flex flex-row-reverse h-52'>
-                <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
-                </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
-                    <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
-                    <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
-                    <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
-                </div>
-            </div>
-            <div className='flex lg:flex-row-reverse h-52'>
-                <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
-                </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
-                    <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
-                    <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
-                    <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
-                </div>
-            </div>
-            <div className='flex flex-row-reverse md:flex-row lg:flex-row-reverse h-52'>
-                <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80")]'>
-                </div>
-                <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
-                    <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>Burger</h1>
-                    <div className='text-gray-500 text-sm mb-5'>Lorem commodi consequuntur nisi facilis, quo recusandae vero perferendis officia!</div>
-                    <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
-                </div>
-            </div>
+  return (
+    <div className='grid grid-cols-1 my-3 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='flex h-52'>
+        <div className='h-full w-1/2 bg-cover bg-center bg-[url("../src/components/images/sp1.jpg")]'></div>
+        <div className='w-1/2 bg-dark h-full p-3 text-left'>
+          <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
+            Mexican Sizzler
+          </h1>
+          <div className='text-gray-500 text-sm mb-5'>
+            Mexican rice served with curry, patty, sautéed veg and fries
+          </div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹499</div>
         </div>
-    )
+      </div>
+      <div className='flex flex-row-reverse md:flex-row h-52'>
+        <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp2.jpg")]'></div>
+        <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+          <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
+            Hot Chocolate
+          </h1>
+          <div className='text-gray-500 text-sm mb-5'>
+            Hot Chocolate with Biscuit Cup
+          </div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹60</div>
+        </div>
+      </div>
+      <div className='flex md:flex-row-reverse lg:flex-row h-52'>
+        <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp3.jpg")]'></div>
+        <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+          <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
+            Burger
+          </h1>
+          <div className='text-gray-500 text-sm mb-5'>
+            Lorem commodi consequuntur nisi facilis, quo recusandae vero
+            perferendis officia!
+          </div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹100</div>
+        </div>
+      </div>
+      <div className='flex flex-row-reverse h-52'>
+        <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp4.jpg")]'></div>
+        <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+          <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
+            Jeera Rice
+          </h1>
+          <div className='text-gray-500 text-sm mb-5'>
+            Hot aromatic rice with cumin tadka
+          </div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹99</div>
+        </div>
+      </div>
+      <div className='flex lg:flex-row-reverse h-52'>
+        <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp5.jpg")]'></div>
+        <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+          <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
+            Plain-chatpata peri peri
+          </h1>
+          <div className='text-gray-500 text-sm mb-5'>
+            Fries with the choice of flavor
+          </div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹129</div>
+        </div>
+      </div>
+      <div className='flex flex-row-reverse md:flex-row lg:flex-row-reverse h-52'>
+        <div className='w-1/2 col-span-1 h-full bg-cover bg-center bg-[url("../src/components/images/sp6.jpg")]'></div>
+        <div className='w-1/2 col-span-1 bg-dark h-full p-3 text-left'>
+          <h1 className='fontmerienda font-bold text-2xl my-2 text-white'>
+            Hakka Noodles
+          </h1>
+          <div className='text-gray-500 text-sm mb-5'>
+            Hakka noodles are Indian-Chinese style noodles made with unleavened
+            refined wheat flour.
+          </div>
+          <div className='text-2xl text-primary-200 font-bold mb-0'>₹169</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MenuDisplay
